d7c2cc87b9b7f90bd481b54dd175cc5f7b623de2
abhishek kumar <abhishekkumar669@gmail.com>
2016-09-14 00:07:04 +0530

    adding insfi signature in application

--- src/main/java/com/insfi/mongoui/Application.java
+++ src/main/java/com/insfi/mongoui/Application.java
@@ -3,6 +3,17 @@ package com.insfi.mongoui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  _            __ _ 
+(_)_ __  ___ / _(_)
+| | '_ \/ __| |_| |
+| | | | \__ \  _| |
+|_|_| |_|___/_| |_|
+
+ * @author abhishek
+ *
+ */
+
 @SpringBootApplication
 public class Application
 {
